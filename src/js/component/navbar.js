3b330720a98664070e59f0faa11f4259f99732ad
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-05-11 22:19:48 -0400

    Decorado navbar y footer con bootstrap

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <div className="container">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <div className="navbar-brand mb-0 h1 d-flex flex-row align-items-center">
+            <i className="fas fa-jedi me-2 fs-2"></i>
+            <h4 className="mb-0">Star Wars</h4>
+          </div>
+        </Link>
+        <div className="ml-auto">
+          <button type="button" className="btn btn-primary">
+            Favorites <span className="badge bg-secondary">4</span>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
